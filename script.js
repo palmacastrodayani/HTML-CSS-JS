158bbd5e18d9f9e0c49e0ee225c82b9726b8e7f4
palmacastrodayani <151578902+palmacastrodayani@users.noreply.github.com>
2024-05-22 17:31:45 +0000

    css

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Definición de variables
-var userName = "Iris"; 
-var userAge = 17; 
-var isStudent = true; 
+var userName = "Iris";
+var userAge = 17;
+var isStudent = true;
+
+var enviarButton = document.getElementById("enviar");
+var massageInput = document.getElementById("massage-input");
+var massageArea = document.getElementById("massage-area");
+
+enviarButton.onclick = function(event) {
+    event.preventDefault();
+    // Append the new message to the existing content with a line break
+    massageArea.innerHTML += massageInput.value + '<br>';
+    // Clear the input field after appending the message
+    massageInput.value = '';
+}
+
 
 function greetUser(name, age) {
-    var greeting = "¡Hola, " + name + "! "; 
+    var greeting = "¡Hola, " + name + "! ";
     if (age >= 18) {
         greeting += "¡Eres mayor de edad!";
     } else {
